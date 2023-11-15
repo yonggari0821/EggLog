a262678ebbbb3c2f49af527c26eb2f141cf19873
no-merit <kewer9024@ajou.ac.kr>
2023-11-15 14:29:44 +0900

    database

---
+++ Egglog_Database.sql
@@ -0,0 +1,54 @@
+CREATE database if NOT EXISTS `egglog`;
+use `egglog`;
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `nickname` varchar(40) NOT NULL,
+  `sex` varchar(40) NOT NULL,
+  `birth` DATE NOT NULL,
+--   `phonenum` varchar(40) NOT NULL,
+--   `public status` boolean NOT NULL,
+  `status_message` varchar(40) DEFAULT NULL,
+  `profile_picture` varchar(100) default NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4;
+
+-- -----------------------------------------------------
+-- Table `diary`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `diary` (
+  `user_id` varchar(40) NOT NULL,
+  `title` VARCHAR(40) NOT NULL,
+  `context` VARCHAR(500) NOT NULL,
+  `diary_date` date NOT NULL,
+  `diary_picture` varchar(100) default NULL,
+  `location` varchar(40) default NULL,
+	PRIMARY KEY (`diary_date`),
+  	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  ) ENGINE = InnoDB;
+  
+-- -----------------------------------------------------
+-- Table `request`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `request` (
+  `request_num` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `from` VARCHAR(40) NOT NULL,
+  `to` varchar(40) NOT NULL
+  ) ENGINE = InnoDB;
+  
+-- -----------------------------------------------------
+-- Table `friends`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `friends` (
+  `my_id` VARCHAR(40) NOT NULL,
+  `friend_id` varchar(40) NOT NULL,
+  PRIMARY KEY (`my_id`,`friend_id`)
+  ) ENGINE = InnoDB;
+  
+select * from user;
+select * from diary;
+select * from request;
+select * from friends;
