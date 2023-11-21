b3fbbc197f8f938f6528a828d0f8d9d78b69318e
Jieun <ije000131@gmail.com>
2023-11-21 09:39:32 +0900

    ads

--- EggLogFrontEnd/src/stores/userStore.js
+++ EggLogFrontEnd/src/stores/userStore.js
@@ -4,7 +4,7 @@ import { ref, computed } from "vue";
 import router from "@/router";
 // import http from "@/util/http-common.js";
 
-const REST_BOARD_API = "http://localhost:8080/api/user";
+const REST_USER_API = "http://localhost:8080/api/user";
 
 export const useUserStore = defineStore("user", () => {
   const user = ref({
@@ -21,9 +21,9 @@ export const useUserStore = defineStore("user", () => {
 
   // 로그아웃하면 localstorage 삭제
   // localstorage에 저장한 id를 기반으로 가져오자.
-  const setLoginUser = function (inputuser) {
+  const setLoginUser = function (userId) {
     axios
-      .get(`${REST_BOARD_API}/${inputuser}`)
+      .get(`${REST_USER_API}/${userId}`)
       .then((response) => {
         user.value = response.data;
         router.push({ name: "MainPage" });
@@ -34,9 +34,32 @@ export const useUserStore = defineStore("user", () => {
       });
   }; //
 
+  // 테스트용;
+  const Login = function (User) {
+    console.log(User);
+    axios
+      .post(`${REST_USER_API}/login`, User)
+      // 트루 반환시 localstorage에 정보 저장,
+      // false면 alert로 알람처리
+      .then((response) => {
+        const user = ref({
+          id: response.data.id,
+          token: response.data.password,
+        });
+        console.log("유저정보는? " + user);
+        localStorage.setItem("userid", user.value.id);
+        localStorage.setItem("accesstoken", user.value.token);
+        router.push({ name: "MainPage" });
+      })
+      .catch((err) => {
+        console.error(err); // Logging the error for debugging
+        alert("로그인에 실패하였습니다.");
+      });
+  }; // 테스트용
 
   const setLogout = function () {
     loginUser.value = false;
   };
 
+  return { user, setLoginUser, Login };
 });
